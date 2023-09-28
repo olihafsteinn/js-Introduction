5df598b6503e2ad917334743293034d37e0bdf0c
olihafsteinn <olihafsteinnh@gmail.com>
2023-09-28 21:24:24 +0000

    added comments

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 
 let countEl = document.getElementById("count-el") // gets the id from h2 tag in html
-console.log(countEl)                              // displayes the html tag into the console to check if its working
+// console.log(countEl)                              displayes the html tag into the console to check if its working
 
 let saveEl = document.getElementById("save-el")   // gets the id from p tag in html
-console.log(saveEl)                               // displayes the html tag into the console to check if its working
+// console.log(saveEl)                               displayes the html tag into the console to check if its working
 
 
 let count = 0                                     // counter starts at 0
